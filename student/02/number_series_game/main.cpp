d7bcdea9feb4f5f0a016be7c8f3c6d00ee8fb05c
Antti-Jussi <antti-jussi.isoviita@tuni.fi>
2020-05-21 11:06:29 +0300

    Lisätty 3:lla jaollisten lukujen käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -7,9 +7,19 @@ int main()
     cout << "How many numbers would you like to have? ";
     int number = 0;
     cin >> number;
-    int n = 0;
+    int n = 1;
     while (n != number)
     {
-        cout << ++n << endl;
+        if (n % 3 == 0)
+        {
+            cout << "zip" << endl;
+
+        }
+        else
+        {
+            cout << n << endl;
+        }
+        ++n;
+
     }
 }
