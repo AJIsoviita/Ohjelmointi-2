0060a6899d4fa7edc50505d9dca9fb6f742fdbbf
Antti-Jussi <antti-jussi.isoviita@tuni.fi>
2020-05-27 16:40:06 +0300

    Molkky t.3.7.1

--- student/03/molkky/player.cpp
+++ student/03/molkky/player.cpp
@@ -1,6 +1,32 @@
 #include "player.hh"
+#include <iostream>
+#include <string>
+using namespace std;
 
-Player::Player()
+Player::Player(string name, int score):
+    name(name), score(score)
 {
+}
 
+string Player::get_name(){
+    return name;
+}
+int Player::get_points(){
+    return score;
+}
+bool Player::has_won()
+{
+    if(score==50)
+        return true;
+    else
+        return false;
+}
+void Player::add_points(int pts)
+{
+    score += pts;
+    if (score > 50)
+    {
+        score = 25;
+        cout << get_name() << "gets penalty points!" << endl;
+    }
 }
--- student/03/molkky/player.hh
+++ student/03/molkky/player.hh
@@ -1,11 +1,21 @@
+#include <string>
 #ifndef PLAYER_HH
 #define PLAYER_HH
+using namespace std;
 
 
 class Player
 {
 public:
-    Player();
+    Player(string name, int score = 0);
+    string get_name();
+    int get_points();
+    void add_points(int pts);
+    bool has_won();
+private:
+    std::string name;
+    int score;
+
 };
 
 #endif // PLAYER_HH
