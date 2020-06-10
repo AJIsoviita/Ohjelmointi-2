e737a4496d2aa6f76a55d3198c27afdbff5c4f01
Antti-Jussi <antti-jussi.isoviita@tuni.fi>
2020-06-10 14:12:19 +0300

    Tehtävä 5.2.1

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Input file: ";
+    string input = "";
+    getline(cin, input);
+    cout << "Output file: ";
+    string output = "";
+    getline(cin, output);
+
+    ifstream input_file(input);
+    ofstream output_file(output);
+
+    if (not input_file)
+        cout << "Error! The file "<< input << " cannot be opened." << endl;
+    else
+    {
+        string row = "";
+        int number = 1;
+        while( getline(input_file, row))
+        {
+            output_file << number << " " << row << endl;
+            number++;
+        }
+        input_file.close();
+        output_file.close();
+    }
+}
